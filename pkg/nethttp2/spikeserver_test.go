0d0fd7f229e783d89173d85817d1a915cf572130
Alan Hemmings <goblinfactory@gmail.com>
2021-11-25 17:26:43 +0000

    add test creating our own handler

--- pkg/nethttp2/spikeserver_test.go
+++ pkg/nethttp2/spikeserver_test.go
@@ -1,16 +1,31 @@
 package nethttp2
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func TestCreatingOurOwnHandler(t *testing.T) {
+	runFor := 2 * time.Second
+	start := time.Now()
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from test."))
+
+		left := runFor - time.Since(start)
+		msg := fmt.Sprintf("Hello from test2. %0.1f seconds left, till test server stops.", left.Seconds())
+		w.Write([]byte(msg))
 	}
 
-	s := http.Server{Addr: ":8081", Handler: handler}
+	s := http.Server{Addr: ":8081", Handler: http.HandlerFunc(handler)}
 
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(runFor))
+
+	go s.ListenAndServe()
+	<-ctx.Done()
+	cancel()
+	s.Close()
 }
